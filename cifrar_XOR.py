af1623b725195f0af9d04f3ba5e227fef77f1695
CarlIFRN <Carloswilliam.felix@gmail.com>
2026-04-27 19:23:05 -0300

    new commit

--- cifrar_XOR.py
+++
@@ -1,8 +0,0 @@
-def cifra(bloco, chave):
-    ind_chave = 0
-    for pos in range(bloco):
-        bloco[pos] = ord(bloco[pos])^ord(chave[ind_chave])
-    ind_chave += 1
-    if ind_chave == len(chave):
-        ind_chave = 0
-        
